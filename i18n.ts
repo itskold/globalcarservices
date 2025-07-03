34bd2a46ae3ce676f9258ac200895bcea180a0e7
itskold <mrd.khattabi@gmail.com>
2025-07-03 20:34:26 +0200

    fix vercel

--- i18n.ts
+++ i18n.ts
@@ -1,2 +1,2 @@
-export { locales, defaultLocale, type Locale } from './lib/i18n-with-firebase'
-export { default } from './lib/i18n-with-firebase' 
+export { locales, defaultLocale, type Locale } from './i18n.config'
+export { default } from './i18n.config' 
