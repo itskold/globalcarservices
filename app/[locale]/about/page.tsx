4c81468a49ad38d283e2a4e553265f4e4c017deb
itskold <mrd.khattabi@gmail.com>
2025-07-03 20:08:36 +0200

    upload firebase pour tout

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,6 +1,9 @@
 import { unstable_setRequestLocale } from 'next-intl/server'
 import { AboutContent } from '@/components/about-content'
 
+// Force dynamic rendering to avoid build-time issues with Firebase
+export const dynamic = 'force-dynamic'
+
 export default function AboutPage({ params: { locale } }: { params: { locale: string } }) {
   unstable_setRequestLocale(locale)
 
